b020080789207d7b16350fe93a4b98928cbc37ad
wlseu <wl_seu@126.com>
2014-11-28 01:10:34 +0800

    https://oj.leetcode.com/problems/min-stack/ https://oj.leetcode.com/problems/merge-intervals/

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from logging import exception
 from Tools.Scripts.treesync import raw_input
 
+
 __author__ = 'wl'
 # from string import Template
 # from math import pi
@@ -79,29 +80,63 @@ __author__ = 'wl'
 ### AC
 
 #--*-- coding:utf-8 --*--
-class MinStack:
-    def __init__(self):
-        self.stack = []
-        self.minStack = []
-    # @param x, an integer
-    # @return an integer
-    def push(self, x):
-        self.stack.append(x)
-        if len(self.minStack) == 0 or x < self.minStack[-1][0]: #小于最小值
-            self.minStack.append((x, 1))
-        elif x == self.minStack[-1][0]: #等于最小值
-            self.minStack[-1] = (x, self.minStack[-1][1] + 1)
-    # @return nothing
-    def pop(self):
-        if self.top() == self.getMin(): #pop的值为最小值
-            if self.minStack[-1][1] > 1:
-                self.minStack[-1] = (self.minStack[-1][0], self.minStack[-1][1] - 1)
+# class MinStack:
+#     def __init__(self):
+#         self.stack = []
+#         self.minStack = []
+#     # @param x, an integer
+#     # @return an integer
+#     def push(self, x):
+#         self.stack.append(x)
+#         if len(self.minStack) == 0 or x < self.minStack[-1][0]: #小于最小值
+#             self.minStack.append((x, 1))
+#         elif x == self.minStack[-1][0]: #等于最小值
+#             self.minStack[-1] = (x, self.minStack[-1][1] + 1)
+#     # @return nothing
+#     def pop(self):
+#         if self.top() == self.getMin(): #pop的值为最小值
+#             if self.minStack[-1][1] > 1:
+#                 self.minStack[-1] = (self.minStack[-1][0], self.minStack[-1][1] - 1)
+#             else:
+#                 self.minStack.pop()
+#         self.stack.pop()
+#     # @return an integer
+#     def top(self):
+#         return self.stack[-1]
+#     # @return an integer
+#     def getMin(self):
+#         return self.minStack[-1][0]
+
+
+#https://oj.leetcode.com/problems/merge-intervals/
+
+class Interval:
+
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+class Solution:
+
+    # @param intervals, a list of Interval
+    # @return a list of Interval
+    def merge(self, intervals):
+
+        if len(intervals) <= 1:
+            return intervals
+
+        intervals.sort(lambda x, y: cmp(x.start, y.start))
+        new_intervals = [ ]
+        last_interval = intervals[0]
+        for i in range(1, len(intervals)):
+            if intervals[i].start >= last_interval.start and intervals[i].start <= last_interval.end:
+                if intervals[i].end >= last_interval.start and intervals[i].end <= last_interval.end:
+                    pass
+                else:
+                    last_interval.end = intervals[i].end
             else:
-                self.minStack.pop()
-        self.stack.pop()
-    # @return an integer
-    def top(self):
-        return self.stack[-1]
-    # @return an integer
-    def getMin(self):
-        return self.minStack[-1][0]
+                new_intervals.append(last_interval)
+                last_interval = intervals[i]
+        new_intervals.append(last_interval)
+
+        return new_intervals
