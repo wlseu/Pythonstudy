04c24b7cb5bf644deee20d4dd33a933d13202f5c
wlseu <wl_seu@126.com>
2014-11-28 01:04:20 +0800

    https://oj.leetcode.com/problems/min-stack/

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from logging import exception
 from Tools.Scripts.treesync import raw_input
 
 __author__ = 'wl'
@@ -28,36 +29,79 @@ __author__ = 'wl'
 #         }
 # request = raw_input('phone num(p) or address(a)?')
 
+#leetcode minstack
 
+# memory exceed
+
+
+# class MinStack:
+#     def __init__(self):
+#         self.stack = []
+#         self.minstack = []
+#
+#
+#     # @param x, an integer
+#     # @return an integer
+#     def push(self, x):
+#         if len(self.stack) == 0:
+#             self.stack.append(x)
+#             self.minstack.append(x)
+#         else:
+#             self.stack.append(x)
+#             if x > self.minstack[-1]:
+#                 y = self.minstack[-1]
+#                 self.minstack.append(y)
+#             else:
+#                 self.minstack.append(x)
+#
+#
+#
+#     # @return nothing
+#     def pop(self):
+#         if len(self.stack) == 0:
+#             raise exception ("stack is empty")
+#         else:
+#             self.minstack.pop()
+#             self.stack.pop()
+#
+#
+#
+#
+#     # @return an integer
+#     def top(self):
+#         return self.stack[-1]
+#
+#     # @return an integer
+#     def getMin(self):
+#         return self.minstack[-1]
+
+
+### AC
+
+#--*-- coding:utf-8 --*--
 class MinStack:
     def __init__(self):
         self.stack = []
-        self.minstack = []
-
-
+        self.minStack = []
     # @param x, an integer
     # @return an integer
     def push(self, x):
-        if len(self.stack) == 0:
-            self.stack.append(x)
-            self.minstack.append(x)
-        else:
-            self.stack.append(x)
-            if x > self.minstack[-1]:
-                self.minstack.append(self.minstack[-1])
-            else:
-                self.minstack.append(x)
-
-
-
+        self.stack.append(x)
+        if len(self.minStack) == 0 or x < self.minStack[-1][0]: #小于最小值
+            self.minStack.append((x, 1))
+        elif x == self.minStack[-1][0]: #等于最小值
+            self.minStack[-1] = (x, self.minStack[-1][1] + 1)
     # @return nothing
     def pop(self):
-
-
+        if self.top() == self.getMin(): #pop的值为最小值
+            if self.minStack[-1][1] > 1:
+                self.minStack[-1] = (self.minStack[-1][0], self.minStack[-1][1] - 1)
+            else:
+                self.minStack.pop()
+        self.stack.pop()
     # @return an integer
     def top(self):
         return self.stack[-1]
-
     # @return an integer
     def getMin(self):
-        return self.minstack[-1]
+        return self.minStack[-1][0]
